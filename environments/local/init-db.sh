a89e181b4fba8a9c8cf527216e11ae2aa9f51f3b
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-01-28 22:10:38 +0100

    Added comments and restructured the script

--- environments/local/init-db.sh
+++ environments/local/init-db.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+# This script is designed for the automated setup of PostgreSQL for Avalanche CMS and Keycloak.
+# During the initialization phase of PostgreSQL, this script is executed, being mounted to /docker-entrypoint-initdb.d/.
+# It securely retrieves user passwords from designated secret files.
+
+
+# Exit on error, unset variables and errors in pipelines
 set -eou pipefail
 
-# returns secret from mounted file content
-# $1: file path to secret file
+
+# Functions
+
+# Retrieves a secret from a given file
+# This function reads the first line of a file and treats it as a secret.
+#
+# Arguments:
+#   $1 - The file path to the secret file. This should be a readable file containing the secret.
+#
+# Returns:
+#   The secret value, or terminates with code 1 if it failed
+#
 get_secret_from_file() {
 
     local file_path="$1"
@@ -23,18 +40,50 @@ get_secret_from_file() {
     }
 }
 
-echo "Creating the 'avalanchecms' database and user, and granting connection privileges and other necessary rights..."
-AVALANCHECMS_SECRET=$(get_secret_from_file "/run/secrets/postgres-avalanchecms-db-user-secret") || exit 1
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE avalanchecms;
-    CREATE USER avalanchecms WITH ENCRYPTED PASSWORD '$AVALANCHECMS_SECRET';
-	GRANT ALL PRIVILEGES ON DATABASE avalanchecms TO avalanchecms;
-EOSQL
+# Creates a new database in PostgreSQL and a user for it. 
+# Uses a secret file to inject the password for the created user.
+# 
+# Arguments:
+#   $1 - Database name. The name of the database to be created.
+#   $2 - Username. The name of the user to be created for managing the database.
+#   $3 - Secret file path. The file path containing the password for the newly created user.
+#
+# Returns:
+#   0 if the database and user were successfully created and configured; 
+#   non-zero on error (e.g., if psql commands fail, or the secret file is unreadable).
+#
+create_db_and_user() {
 
-echo "Creating the 'keycloak' database and user, and granting connection privileges and other necessary rights..."
-KEYCLOAK_SECRET=$(get_secret_from_file "/run/secrets/postgres-keycloak-db-user-secret") || exit 1
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE keycloak;
-    CREATE USER keycloak WITH ENCRYPTED PASSWORD '$KEYCLOAK_SECRET';
-	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+    local db_name=$1
+    local user_name=$2
+    local secret_file=$3
+
+    echo "Creating the '$db_name' database and '$user_name' user, and granting connection privileges and other necessary rights..."
+
+    # Retrieve the secret password
+    local db_password=$(get_secret_from_file "$secret_file") || exit 1
+
+    # execute PostgreSQL commands
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
+        CREATE USER $user_name WITH PASSWORD '$db_password';
+        CREATE DATABASE $db_name;
+        ALTER DATABASE $db_name OWNER TO $user_name;
+        GRANT ALL PRIVILEGES ON DATABASE $db_name TO $user_name;
+        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $user_name;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $user_name;
 EOSQL
+}
+
+
+# Main
+main() {
+
+    # Create databases and users for 'avalanchecms' and 'keycloak'
+    create_db_and_user "avalanchecms" "avalanchecms" "/run/secrets/postgres-avalanchecms-db-user-secret"
+    create_db_and_user "keycloak" "keycloak" "/run/secrets/postgres-keycloak-db-user-secret"
+}
+
+# Check if the script is being sourced or executed
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
