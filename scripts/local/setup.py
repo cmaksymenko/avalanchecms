2f2252f5a37e9609eb5967f03f6262e7bdae682b
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-01-29 23:07:15 +0100

    Updated documentation

--- scripts/local/setup.py
+++ scripts/local/setup.py
@@ -1,4 +1,10 @@
-# main setup script
+# Avalanche CMS Local Stack setup script
+#
+# This script sets up a local development environment for Avalanche CMS. It includes functionalities to:
+# - Prompt the user to either enter a secret or generate a strong secure secret automatically, for all stack components
+# - Create secret files in a designated '.secrets' directory within the project's root folder
+# - Offer an automated mode for generating all secrets without user input, triggered by the '-a/--auto' argument
+# - The script assumes its location in 'scripts/local' and calculates the project root from there
 
 import os
 import random
@@ -24,14 +30,32 @@ secrets = [
     {"name": "Keycloak Admin User", "file": "keycloak-admin-user-secret.env"}
 ]
 
+# Finds the root directory of the project
+#
+# Args: current_file (str): The file path of the current script
+# Returns: root directory path of the project
+#
 def find_project_root(current_file):
-    # Assuming the script is in 'scripts/dev-setup'
+    # Assuming the script is in 'scripts/local'
     # Adjust the number of os.path.dirname calls based on actual script location
     return os.path.dirname(os.path.dirname(os.path.dirname(current_file)))
 
+# Generates a random password
+#
+# Args: length (int, optional): The length of the password to be generated
+# Returns: random password string
+#
 def generate_random_password(length=SECRET_DEFAULT_LEN):
     return ''.join(random.choice(SECRET_CHAR_POOL) for i in range(length))
 
+# Prompts for a secret or generates one automatically
+#
+# Args:
+#   description (str): Description of the secret
+#   auto (bool, optional): If True, generates a secret automatically. Defaults to False.
+#
+# Returns: User-provided or randomly generated secret
+#
 def prompt_for_secret(description, auto=False):
     if not auto:
         user_input = input(f"Enter secret for {description} (press enter for random): ").strip()
@@ -41,7 +65,14 @@ def prompt_for_secret(description, auto=False):
     print(f"Generating a random secret for {description}...")
     return generate_random_password()
 
-
+# Creates a secret file if it doesn't already exist
+#
+# Args:
+#   path (str): The file path where the secret should be saved
+#   content (str): The content to write in the secret file
+#
+# Note: If the file already exists, creation is skipped
+#
 def create_secret_file(path, content):
     if not os.path.exists(path):
         with open(path, 'w') as file:
@@ -50,11 +81,11 @@ def create_secret_file(path, content):
     else:
         print(f"Secret file already exists: {path}, skipping...")
 
+# Main
 def main():
 
     parser = argparse.ArgumentParser(description="Avalanche CMS setup script for local development.")
     parser.add_argument('-a', '--auto', action='store_true', help='Enable automated mode with default settings.')
-    
     args = parser.parse_args()
 
     print(f"{'Automated' if args.auto else 'Interactive'} mode enabled.")
