3013787b151f1fbf38632b101c0ac09194d39904
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-01-29 12:15:49 +0100

    Refactored Keycloak environment setup and password handling, using now an 'urlencode' function to encode special characters of used secret for KC_DB_URL.

--- environments/local/keycloak-env-loader.sh
+++ environments/local/keycloak-env-loader.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+
+# Exit on error, unset variables and errors in pipelines
 set -eou pipefail
 
-# returns secret from mounted file content
+
+# Functions
+
+# Returns secret from mounted file content
 # $1: file path to secret file
 get_secret_from_file() {
 
@@ -23,19 +29,49 @@ get_secret_from_file() {
     }
 }
 
-echo "Setting KEYCLOAK_ADMIN_PASSWORD..."
-export KEYCLOAK_ADMIN_PASSWORD="$(get_secret_from_file "${KEYCLOAK_ADMIN_PASSWORD_FILE}")"
+# Encodes a string for safe use in a URL by percent-encoding characters that are not URL-safe
+# $1 - The input string to be encoded.
+# Returns the encoded string suitable for use in a URL
+#
+urlencode() {
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+}
 
-echo "Setting KC_DB and KC_DB_URL for PostgreSQL connection..."
-export KC_DB="postgres"
-echo "KC_DB: $KC_DB"
-export KC_DB_URL="jdbc:postgresql://${KC_DB_URL_HOST}:${KC_DB_URL_PORT}/${KC_DB_URL_DATABASE}?user=${KC_DB_USERNAME}&password=$(get_secret_from_file "${KC_DB_PASSWORD_FILE}")&ssl=${KC_DB_URL_SSL_ENABLED}"
-echo "KC_DB_URL: jdbc:postgresql://${KC_DB_URL_HOST}:${KC_DB_URL_PORT}/${KC_DB_URL_DATABASE}?user=${KC_DB_USERNAME}&password=*****&ssl=${KC_DB_URL_SSL_ENABLED}"
 
-if [ -n "$@" ]; then
-  echo "Starting Keycloak with arguments: $@"
-else
-  echo "Starting Keycloak..."
+# Main
+main() {
+
+    # set Keycloak env vars
+
+    echo "Setting KEYCLOAK_ADMIN_PASSWORD..."
+    export KEYCLOAK_ADMIN_PASSWORD="$(get_secret_from_file "${KEYCLOAK_ADMIN_PASSWORD_FILE}")"
+
+    echo "Setting KC_DB and KC_DB_URL for PostgreSQL connection..."
+    export KC_DB="postgres"
+    echo "KC_DB: $KC_DB"
+
+    export KC_DB_URL="jdbc:postgresql://${KC_DB_URL_HOST}:${KC_DB_URL_PORT}/${KC_DB_URL_DATABASE}?user=${KC_DB_USERNAME}&password=$(urlencode "$(get_secret_from_file "${KC_DB_PASSWORD_FILE}")")&ssl=${KC_DB_URL_SSL_ENABLED}"
+    echo "KC_DB_URL: jdbc:postgresql://${KC_DB_URL_HOST}:${KC_DB_URL_PORT}/${KC_DB_URL_DATABASE}?user=${KC_DB_USERNAME}&password=*****&ssl=${KC_DB_URL_SSL_ENABLED}"
+
+    # start Keycloak
+
+    if [ -n "$@" ]; then
+    echo "Starting Keycloak with arguments: $@"
+    else
+    echo "Starting Keycloak..."
+    fi
+
+    exec /opt/keycloak/bin/kc.sh "$@"
+}
+
+# Check if the script is being sourced or executed
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
 fi
-
-exec /opt/keycloak/bin/kc.sh "$@"
