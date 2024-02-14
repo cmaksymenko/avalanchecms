ebbf14d101549f9245d28695d667b777e6c39d3f
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-02-14 23:04:42 +0100

    Separate Keycloak Dockerfile with installed 'jq'. Keycloak script updated for dynamic 'createdTimestamp' generation.

--- environments/local/scripts/keycloak-config.sh
+++ environments/local/scripts/keycloak-config.sh
@@ -29,3 +29,45 @@ if [ -z "${AV_NO_DLR_SUB}" ]; then
 else
     echo "Dollarsign substitution skipped."
 fi
+
+echo "Preparing Keycloak realm config."
+
+input_file="/tmp/avalanchecms/keycloak-realm-config.orig.json"
+
+current_epoch_ms=$(date +%s%3N)
+
+echo "input_file: $input_file"
+echo "current_epoch_ms: $current_epoch_ms"
+
+find /tmp -type f -name 'avalanchecms.*' -exec rm {} +
+
+tmp_output_file=$(mktemp /tmp/avalanchecms.XXXXXX)
+
+# Set users createdTimestamp to current epoch
+jq --arg epoch "$current_epoch_ms" '
+  if .users and (.users | type) == "array" and (.users | length) > 0 then
+    .users |= map(
+      if .createdTimestamp then
+        .createdTimestamp = ($epoch | tonumber)
+      else
+        . + {"createdTimestamp": ($epoch | tonumber)}
+      end
+    )
+  else
+    .
+  end
+' "$input_file" > "$tmp_output_file"
+
+if [ $? -eq 0 ]; then
+  echo "Set user createdTimestamp to current epoch."
+else
+  echo "Failed to set user createdTimestamp." >&2
+  exit 1
+fi
+
+echo "Copying modified Keycloak config to /opt/keycloak/data/import/keycloak-realm-config.json"
+
+mkdir -p /opt/keycloak/data/import/
+mv ${tmp_output_file} /opt/keycloak/data/import/keycloak-realm-config.json
+
+echo "Keycloak successfully configured."
