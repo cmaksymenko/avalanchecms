0d1e289811629526347cc874ac9e51837334bd39
Christian Maksymenko <christian.maksymenko@gmail.com>
2024-02-21 22:04:06 +0100

    Updated pgAdmin settings for a more frictionless DX in local dev environment

--- environments/local/config/config_local.py
+++ environments/local/config/config_local.py
@@ -1,22 +1,26 @@
 import os
 
 SERVER_MODE = True
-MASTER_PASSWORD_REQUIRED = True
-DEBUG = True
 AUTHENTICATION_SOURCES = ['oauth2', 'internal']
+MFA_ENABLED = False
 OAUTH2_AUTO_CREATE_USER = True
+MASTER_PASSWORD_REQUIRED = False
+MAX_LOGIN_ATTEMPTS = 0
+DEBUG = True
+AUTO_DISCOVER_SERVERS = False
+
 OAUTH2_CONFIG = [{
     'OAUTH2_NAME': 'keycloak',
     'OAUTH2_DISPLAY_NAME': 'keycloak',
     'OAUTH2_CLIENT_ID': 'pgadmin',
     'OAUTH2_TOKEN_URL': 'http://host.docker.internal:8080/realms/avalanchecms/protocol/openid-connect/token',
     'OAUTH2_AUTHORIZATION_URL': 'http://host.docker.internal:8080/realms/avalanchecms/protocol/openid-connect/auth',
-    'OAUTH2_SCOPE': 'openid email profile',
     'OAUTH2_SERVER_METADATA_URL': 'http://host.docker.internal:8080/realms/avalanchecms/.well-known/openid-configuration',
-    'OAUTH2_AUTO_CREATE_USER': False,
-    'OAUTH2_SSL_CERT_VERIFICATION': False,
     'OAUTH2_USERINFO_ENDPOINT': 'http://host.docker.internal:8080/realms/avalanchecms/protocol/openid-connect/userinfo',
-    'OAUTH2_API_BASE_URL': 'http://host.docker.internal:8080/realms/avalanchecms'
+    'OAUTH2_API_BASE_URL': 'http://host.docker.internal:8080/realms/avalanchecms',
+    'OAUTH2_SCOPE': 'openid email profile',
+    'OAUTH2_AUTO_CREATE_USER': False,
+    'OAUTH2_SSL_CERT_VERIFICATION': False
 }]
 
 # Add OAUTH2_CLIENT_SECRET only if PGADMIN_OAUTH2_CLIENT_SECRET is set
